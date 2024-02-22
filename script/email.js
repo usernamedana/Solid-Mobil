69b2f884a34704e7c72b2d646357abae88c5c3ff
Tamazlîcari Maria <140591468+mashamaria999@users.noreply.github.com>
2024-02-22 20:08:28 +0200

    Add files via upload

--- script/email.js
+++ script/email.js
@@ -1,10 +1,20 @@
-function SendMail() {
+date
   var params = {
     from_name: document.getElementById("nume").value,
     email_id: document.getElementById("email_id").value,
-    message: document.getElementById("mesaj")
+    message: document.getElementById("message")
   };
-  elsemailjs.send("service_a7zdoof", "template_i41ckj9", params).then(function (res){
-    alert("Succes!" + res.status);
-  })
+function SendMail() { 
+  if (document.getElementById("formular").checkValidity() == false) {
+    document.getElementById("formular").reportValidity();
+  }
+
+  elsemailjs.send("service_a7zdoof", "template_i41ckj9", date).then(function (res){
+    alert("Mesajul a fost trimis.")
+    document.getElementById("formular").reset();
+  },function (error) {
+    alert("Eroare la transmitere. Contactati programatorul")
+    console.log(error);
+  }
+  )
 }
