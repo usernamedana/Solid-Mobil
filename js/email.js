7f00d0fe2e7beb6da4b082cb370d314a4e3e1042
usernamedana <152792768+usernamedana@users.noreply.github.com>
2024-02-25 13:35:54 +0200

    Add files via upload

--- js/email.js
+++ js/email.js
@@ -1,25 +1,4 @@
 
-
-// date
-//   var params = {
-//     from_name: document.getElementById("nume").value,
-//     email_id: document.getElementById("email_id").value,
-//     message: document.getElementById("message")
-//   };
-// function SendMail() { 
-//   if (document.getElementById("formular").checkValidity() == false) {
-//     document.getElementById("formular").reportValidity();
-//   }
-
-//   elsemailjs.send("service_a7zdoof", "template_i41ckj9", date).then(function (res){
-//     alert("Mesajul a fost trimis.")
-//     document.getElementById("formular").reset();
-//   },function (error) {
-//     alert("Eroare la transmitere. Contactati programatorul")
-//     console.log(error);
-//   }
-//   )
-// }
 emailjs.init("zC-2UXQzoyM_zCFOh");
 function send() {
 
